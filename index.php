15e708613bdfc51dca3271228d015e99e5de2eeb
perkles <adller676@gmail.com>
2021-09-09 09:32:00 -0300

    comit inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Inscrição</title>
+</head>
+<body>
+    <h1>FORMUÁRIO DE INSCRIÇÃO DE COMPETIDORES</h1>
+
+    <form action="struct.php" method="post">
+        <p>Seu nome: <input type="text" name="nome" /></p>
+        <p>Sua idade: <input type="text" name="idade"></p>
+        <p><input type="submit" value="Send"></p>
+    </form>
+</body>
+</html>
